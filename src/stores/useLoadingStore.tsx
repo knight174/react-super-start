3c2325cbfd4b668992bb32f78e87a9a16b0fda46
pinchao <cpc4253@163.com>
2023-07-23 17:36:41 +0800

    添加loading 状态设置store

---
+++ src/stores/useLoadingStore.tsx
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+interface Loading {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+}
+export const useLoadingStore = create<Loading>((set) => ({
+  visible: false,
+  setVisible: (visible: boolean) => {
+    set({ visible });
+  },
+}));
