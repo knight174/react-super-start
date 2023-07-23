f8de91d1b003eabd7f9215356699cc0bf1191a01
pinchao <cpc4253@163.com>
2023-07-23 17:36:16 +0800

    添加 isDev 变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const generateConfig = (env) => {
+  const { command } = env;
+
+  return {
+    // 定义全局环境变量
+    define: {
+      // 是否为开发环境
+      isDev: command === "serve",
+    },
+    plugins: [react()],
+  };
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(generateConfig);
